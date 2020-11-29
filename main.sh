6efd7d12a7c67b6645705b6e07d0b9d2bfeb1f7f
João Dias <github.stepgrandmother@aleeas.com>
2020-11-29 17:04:42 +0000

    Chang hostname. Add new user.

--- main.sh
+++ main.sh
@@ -9,3 +9,11 @@ grep -qxF "interface $interface" /etc/dhcpcd.conf || echo "interface $interface"
 grep -qxF "static ip_address=$static_ip/24" /etc/dhcpcd.conf || echo "static ip_address=$static_ip/24" >> /etc/dhcpcd.conf
 grep -qxF "static routers=$router_ip" /etc/dhcpcd.conf || echo "static routers=$router_ip" >> /etc/dhcpcd.conf
 grep -qxF "static domain_name_servers=$dns_server" /etc/dhcpcd.conf || echo "static domain_name_servers=$dns_server" >> /etc/dhcpcd.conf
+
+# change hostname
+sudo sed -i "s/raspberrypi/$new_hostname/" /etc/hosts
+sudo sed -i "s/raspberrypi/$new_hostname/" /etc/hostname
+
+# create new user
+sudo adduser "$new_user"
+sudo usermod -a -G adm,dialout,cdrom,sudo,audio,video,plugdev,games,users,input,netdev,gpio,i2c,spi "$new_user"
